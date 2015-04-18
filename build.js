8c343277cd1cd32c1f3c40971bd6f2735a5be9b5
Clay Carpenter <claycarpenter@gmail.com>
2015-04-18 20:57:27 +0000

    Removes test plugin.

--- build.js
+++ build.js
@@ -2,33 +2,11 @@ var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
     templates = require('metalsmith-templates');
 
-var logger = function() {
-    return function(files, metalsmith, done) {
-        // console.log('Files count: ' + JSON.stringify(files));
-        
-        setImmediate(done);
-        
-        for (var fileName in files) {
-            if (files.hasOwnProperty(fileName)) {
-                console.log('\nFound file: ' + fileName);    
-                var contents = files[fileName].contents.toString();
-                
-                console.log('Contents:\n' + contents);
-                
-                contents = contents.replace(/My Blog/, 'My Other Blog');
-                
-                files[fileName].contents = new Buffer(contents);
-            };
-        }
-    };
-};
-
 Metalsmith(__dirname)
     .source('./src/')
     .destination('./build/')
     .use(markdown())
     .use(templates('handlebars'))
-    .use(logger())
     .build(function(err, files) {
         console.log('Build function...');
         
