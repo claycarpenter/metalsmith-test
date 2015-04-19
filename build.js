c5ae7ce53c7940c1fd87accc263e209edcd2ddd3
Clay Carpenter <claycarpenter@gmail.com>
2015-04-19 20:00:15 +0000

    Adds initial version of Jade template plugin.

--- build.js
+++ build.js
@@ -1,12 +1,36 @@
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
-    templates = require('metalsmith-templates');
+    jade = require('jade'),
+    path = require('path'),
+    fs = require('fs');
+
+function metalsmithJade () {
+    return function (files, metalsmith, done) {
+        Object.keys(files).forEach(function (filename) {
+            var file = files[filename];
+            
+            console.log('File template value:', file.template, 'path:', path.resolve(file.template));
+            
+            var templateFilePath = path.resolve(file.template);
+            var templateContents = fs.readFileSync(templateFilePath, 'utf8').toString();
+            
+            var templateFn = jade.compile(templateContents, {pretty: true, filename: filename});
+            var locals = Object.create(null);
+            locals.data = file;
+            var htmlOutput = templateFn(locals);
+            
+            file.contents = new Buffer(htmlOutput);
+        });
+        
+        done();
+    };
+}
 
 Metalsmith(__dirname)
     .source('./src/')
     .destination('./output/')
     .use(markdown())
-    .use(templates('handlebars'))
+    .use(metalsmithJade())
     .build(function(err, files) {
         console.log('Build function...');
         
