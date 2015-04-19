ac179829c2720b5d8a2fb584afc87abaa1ff70fd
Clay Carpenter <claycarpenter@gmail.com>
2015-04-19 20:31:22 +0000

    Moves Jade templater Metalsmith plugin to own file.

--- build.js
+++ build.js
@@ -1,36 +1,12 @@
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
-    jade = require('jade'),
-    path = require('path'),
-    fs = require('fs');
-
-function metalsmithJade () {
-    return function (files, metalsmith, done) {
-        Object.keys(files).forEach(function (filename) {
-            var file = files[filename];
-            
-            console.log('File template value:', file.template, 'path:', path.resolve(file.template));
-            
-            var templateFilePath = path.resolve(file.template);
-            var templateContents = fs.readFileSync(templateFilePath, 'utf8').toString();
-            
-            var templateFn = jade.compile(templateContents, {pretty: true, filename: filename});
-            var locals = Object.create(null);
-            locals.data = file;
-            var htmlOutput = templateFn(locals);
-            
-            file.contents = new Buffer(htmlOutput);
-        });
-        
-        done();
-    };
-}
+    jadeTemplater = require('./jade-templater.js');
 
 Metalsmith(__dirname)
     .source('./src/')
     .destination('./output/')
     .use(markdown())
-    .use(metalsmithJade())
+    .use(jadeTemplater())
     .build(function(err, files) {
         console.log('Build function...');
         
